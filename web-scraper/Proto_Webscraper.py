541592c9bc65a6274cb4e7cb0fbde258c27b09a1
BenjiLuo <56556840+BenjiLuo@users.noreply.github.com>
2022-12-07 14:30:07 -0500

    Updated web scraper to scrape for p tags in div classes
    Uses BeautifulSoup on Meta Platforms wikipedia page to isolate html content specifically for div classes and p tags.

--- web-scraper/Proto_Webscraper.py
+++ web-scraper/Proto_Webscraper.py
@@ -1,13 +1,18 @@
-
 from bs4 import BeautifulSoup
 import urllib.request as request
 
-url ="https://en.wikipedia.org/wiki/List_of_terms_relating_to_algorithms_and_data_structures"
+url ="https://en.wikipedia.org/wiki/Meta_Platforms"
 headers = {'user-agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.3'}
 
 # with urllib.request.urlopen("https://en.wikipedia.org/wiki/List_of_terms_relating_to_algorithms_and_data_structures") as url:
 req = request.Request(url, headers=headers)
 data = request.urlopen(req).read()
 
-soup = BeautifulSoup(data, "html.parser")
-print(soup)
+soup= BeautifulSoup(data, "html.parser")
+main_content= soup.find("div", attrs= {"id" : "mw-content-text"})
+lists = main_content.find_all("p")
+str = ""
+for list in lists:
+    info= list.text
+    str+=info
+print(str)

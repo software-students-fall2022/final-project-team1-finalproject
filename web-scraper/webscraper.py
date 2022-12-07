58feb03a9ca6706d08923dbd7c009828c6938de9
JonasonWu <jw5911@nyu.edu>
2022-12-07 18:02:22 -0500

    Completed the web scraping up to the part where there is a word_freq dictionary produced

--- web-scraper/webscraper.py
+++ web-scraper/webscraper.py
@@ -2,6 +2,8 @@
 from bs4 import BeautifulSoup
 from urllib import request
 import requests
+from requests.exceptions import ReadTimeout
+# import re
 from googlesearch import search
 
 class WebScrape:
@@ -11,75 +13,185 @@ class WebScrape:
 
     @staticmethod
     def get_google_search_results(user_input: str):
-        '''Given the user input, return a Generator of url string results based on the search'''
+        '''Given the user input, return a Generator of url string results based on the google search'''
         return search(user_input, tld="co.in")
 
     @staticmethod
     def generate_google_url(user_input: str) -> str:
-        '''Given the user input, returns the string for the search query'''
+        '''Given the user input, returns the string for the google search query
+        
+        @deprecated no longer needed
+        '''
         return WebScrape.search_engine + user_input
 
     @staticmethod
     def create_request(url: str) -> request:
-        '''Given a url, returns a request for the url'''
+        '''Given a url, returns a request for the url
+
+        @deprecated no longer used
+        '''
         return request.Request(url, headers=WebScrape.headers)
 
     @staticmethod
     def get_data(req: request) -> str:
-        '''Given a Request, returns the data fetched from the Request'''
+        '''Given a Request, returns the data fetched from the Request
+        
+        @deprecated no longer used
+        '''
         return request.urlopen(req).read()
 
     @staticmethod
     def get_requests(url: str):
         '''Uses the requests module to perform a fetch of data based on the url'''
-        return requests.get(url)
+        return requests.get(url, headers=WebScrape.headers, timeout=3)
 
     @staticmethod
-    def parse_data(data: str) -> BeautifulSoup:
+    def make_soup(data: str) -> BeautifulSoup:
         '''Given data, parses the data into a BeautifulSoup object'''
         return BeautifulSoup(data, WebScrape.parser)
-    
+
+class WebScrapeCleaner:
+
     @staticmethod
     def clean_dom(dom: BeautifulSoup) -> BeautifulSoup:
-        '''Given the dom, remove all the content of the following tags:
-        script
+        '''Given the dom, remove all of the following tags:
+        script 
         head
+        style
+        footer
         '''
-        content = dom.find_all('script, head')
+        content = dom.find_all(['script', 'head', 'style', 'footer'])
         for e in content:
             e.clear()
         return dom
 
+    # Note: Might want to try making this better using regex
+    @staticmethod
+    def clean_string(string: str) -> str:
+        '''Cleans up the string by separating the words based on capitalization.
+        '''
 
-class WebScrapeProcedures:
+        s = " "
+        for c in string:
+            if c.isupper() and not s[-1].isupper():
+                s += ' '
+            s += c
+        # " ".join([s for s in re.split("([A-Z][^A-Z]*)", string) if s])
+        return s
+
+    # Note: Might want to add the stop words into this method to remove them.
+    @staticmethod
+    def include_word(word):
+        '''Determines whether the word should be included in the frequency list.
+        True for yes, False for no.
+        '''
+        if len(word) > 30:
+            return False
+        elif len(word) < 4:
+            return False
+        elif not word.isalpha():
+            return False
+        return True
+
 
     @staticmethod
-    def procedure(user_input):
-        google_dom = WebScrape.parse_data(WebScrape.get_data(WebScrape.create_request(WebScrape.generate_google_url(user_input))))
-        #print(google_dom)
+    def refactor_weird_words(words: list[str]) -> list[str]:
+        '''Given an array of words, returns a new array of only words that are valid
+        '''
+        refactored = []
+        for word in words:
+            if WebScrapeCleaner.include_word(word):
+                refactored.append(word)
+        return refactored
+
+
+class WebScrapeHelper:
     @staticmethod
-    def procedure1(user_input):
-        words = {}
+    def stop_condition(words: dict) -> bool:
+        '''If the number of distinct words and the frequency of the most common word 
+        is above a certain threshold, returns True. Otherwise, false is returned.
+        '''
+        if len(words.keys()) > 30 and max(words.values()) > 200:
+            return True
+        return False
+    
+    @staticmethod
+    def generate_word_freq_dict(words: list[str]) -> dict[str, int]:
+        '''Given a list of strings, create a word freq dictionary'''
+        word_freq = {}
+        for word in words:
+            if word_freq.get(word, None) == None:
+                word_freq[word] = 1
+            else:
+                word_freq[word] += 1
+        return word_freq
+
+    @staticmethod
+    def make_word_freq_dict(data: str) -> dict[str, int]:
+        '''Given the result of the requested url as a string, returns a word freq 
+        dictionary after parsing out the unnecessary words.
+        '''
+
+        # convert the html string into a soup
+        # print("Making soup")
+        dom = WebScrape.make_soup(data)
+
+        # clean up the dom by removing unnecessary text inside specific tags
+        # print("Cleaning dom")
+        dom = WebScrapeCleaner.clean_dom(dom)
+
+        # clean up the string of the remaining text of the beautiful soup
+        # print("Cleaning string")
+        word_string = WebScrapeCleaner.clean_string(dom.text)
+
+        # split by whitespace
+        # print("Spliting by whitespace")
+        words = word_string.split()
+
+        # refactor any words that are not good (i.e., likely to be an invalid word)
+        # print("Refactoring words")
+        words = WebScrapeCleaner.refactor_weird_words(words)
+        
+        # Generate the word freq dictionary and return it
+        # print("Generating word freq dict for", len(words), "words")
+        return WebScrapeHelper.generate_word_freq_dict(words)
+    
+    @staticmethod
+    def combine_word_freq_dicts(dict1: dict[str, int], dict2: dict[str, int]) -> dict[str, int]:
+        '''Adds all words of dict2 into dict1 and returns dict1.
+        '''
+        for word in dict2.keys():
+            if dict1.get(word, None) == None:
+                dict1[word] = 1
+            else:
+                dict1[word] += dict2[word]
+        return dict1
+
+class WebScrapeProcedures:    
+
+    @staticmethod
+    def procedure_1(user_input):
+        word_freq = {}
         url_generator = WebScrape.get_google_search_results(user_input)
         for url in url_generator:
-            req = WebScrape.get_requests(url)
-            print("Status: ", req)
-            print(req.text[:200])
-            dom = WebScrape.parse_data(req.text)
-            dom = WebScrape.clean_dom(dom)
-            print(dom.find('head'))
+            print("Current url: ", url)
+            try:
+                req = WebScrape.get_requests(url)
+            except ReadTimeout as t:
+                print("Timed out:", t)
+                continue
+            print("Status: ", req.status_code)
+            if (req.status_code != 200):
+                print("Error in request. Status code:", req.status_code)
+                continue
+            else:
+                new_word_freq = WebScrapeHelper.make_word_freq_dict(req.text)
+                WebScrapeHelper.combine_word_freq_dicts(word_freq, new_word_freq)
+                if (WebScrapeHelper.stop_condition(word_freq)):
+                    break
 
-            # print(dom[:50])
-            input()
+        print("Number of unique words:", len(word_freq.keys()))
+        print("Highest freq word:", max(word_freq.keys(), key=lambda k: word_freq[k]), max(word_freq.values()))
+        print("Longest word length:", max(word_freq.keys(), key=lambda k: len(k)))
 
-            
-        
-WebScrapeProcedures.procedure1("Hello")
-
-# print(WebScrape.get_google_search_results("HI"))
-        
-    
-
-
-# with urllib.request.urlopen("https://en.wikipedia.org/wiki/List_of_terms_relating_to_algorithms_and_data_structures") as url:
-
+WebScrapeProcedures.procedure_1("Walkie talkie")

05832a47efaf2ba887e628d254314f8c677709a7
markizenlee <xc2097@nyu.edu>
2022-12-08 02:28:40 -0500

    Connect to database

--- web-app/app.py
+++ web-app/app.py
@@ -10,7 +10,21 @@ if os.getenv('FLASK_ENV', 'development') == 'development':
     # turn on debugging, if in development
     app.debug = True # debug mnode
 
-def configure_routes():
+# connect to the database
+
+database = None
+cxn = pymongo.MongoClient(os.getenv('MONGO_URI'), serverSelectionTimeoutMS=5000)
+try:
+    # verify the connection works by pinging the database
+    cxn.admin.command('ping') # The ping command is cheap and does not require auth.
+    database = cxn[os.getenv('MONGO_DBNAME')] # store a reference to the database
+    print(' *', 'Connected to MongoDB!') # if we get here, the connection worked!
+except Exception as e:
+    # the ping command failed, so the connection is not available.
+    print(' *', "Failed to connect to MongoDB at", os.getenv('MONGO_URI'))
+    print('Database connection error:', e) # debug
+
+def configure_routes(db):
     # set up a web app with correct routes
     app = Flask(__name__)
     @app.route('/')
@@ -29,7 +43,7 @@ def configure_routes():
 
     return app
 
-app = configure_routes()
+app = configure_routes(db = database)
 
 if __name__ == "__main__":
     app.run(debug=True)

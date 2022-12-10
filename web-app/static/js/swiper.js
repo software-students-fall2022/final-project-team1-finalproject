9eddfaf31b31380f2b0020bbb4f426433fe40ec1
Brian Lin <blin00712@gmail.com>
2022-12-09 23:53:42 -0500

    12/9-Brian-right side styles

--- web-app/static/js/swiper.js
+++ web-app/static/js/swiper.js
@@ -1,14 +1,17 @@
 const swiper = new Swiper('.swiper-container', {
   // Optional parameters
+  init: true,
   effect: "coverflow",
   grabCursor: true,
-  // loop: true,
+  loop: true,
+  preloadImages: true,
   // pagination
   pagination: {
     el: '.swiper-pagination',
     clickable: true
   },
   centeredSlides: true,
+  slidesPerView: 'auto',
   // navigation: {
   //   nextEl: '.swiper-button-next',
   //   prevEl: '.swiper-button-prev',

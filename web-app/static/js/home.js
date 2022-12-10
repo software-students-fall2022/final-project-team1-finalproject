f9f68a1fb2c6c4c0d7abb6468629eab700117e47
Brian Lin <blin00712@gmail.com>
2022-12-09 23:27:02 -0500

    12/9-Brian-featured-images-anims

--- web-app/static/js/home.js
+++ web-app/static/js/home.js
@@ -1,19 +1,48 @@
 // Retreive DOM elements
+// left elements
 const bg = document.getElementById("bg");
+const leftContainer = document.getElementById("left");
+const appDetails = document.getElementById("app_detail")
 const searchInput = document.getElementById("search_form");
+
+// right elements
+const rightContainer = document.getElementById("right");
+const featureTitle = document.getElementById("feature_title");
+const featuredImages = document.getElementById("featured_images").getElementsByClassName("swiper-slide");
+console.log("featured Images: ", featuredImages)
 
 let mouseOverSearch = false;
 
-// Event listeners for Search element
+// Event listeners for elements on left side
 searchInput.addEventListener("mouseover", e => {
-    console.log("mouse over search")
     bg.style.filter = "blur(4px)";
     bg.style.transform = "scale(1.1)";
 })
 
 searchInput.addEventListener("mouseleave", e => {
-    console.log("mouse leave search")
     bg.style.filter = "none";
     bg.style.transform = "scale(1)";
 })
 
+leftContainer.addEventListener("mouseover", e => {
+    appDetails.style.transform = "scale(1.1)";
+})
+
+leftContainer.addEventListener("mouseleave", e => {
+    appDetails.style.transform = "scale(1)";
+})
+
+// Event listeners for elements on right side
+rightContainer.addEventListener("mouseover", e => {
+    featureTitle.style.transform = "scale(1.1)";
+})
+
+rightContainer.addEventListener("mouseleave", e => {
+    featureTitle.style.transform = "scale(1)";
+})
+
+// featuredImages.forEach(image => {
+//     image.addEventListener("mouseover", e => {
+
+//     })
+// })
--- web-app/static/js/swiper.js
+++ web-app/static/js/swiper.js
@@ -1,17 +1,17 @@
 const swiper = new Swiper('.swiper-container', {
   // Optional parameters
-  effect: "cards",
+  effect: "coverflow",
   grabCursor: true,
-  loop: true,
+  // loop: true,
   // pagination
   pagination: {
     el: '.swiper-pagination',
     clickable: true
   },
   centeredSlides: true,
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
+  // navigation: {
+  //   nextEl: '.swiper-button-next',
+  //   prevEl: '.swiper-button-prev',
+  // },
 
 });

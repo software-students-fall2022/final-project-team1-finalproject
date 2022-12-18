6423a536ed14b004c3eecfff72bd731738b863e0
markizenlee <xc2097@nyu.edu>
2022-12-18 10:55:55 -0500

    adding route tests

--- web-app/tests/test_app.py
+++ web-app/tests/test_app.py
@@ -5,7 +5,32 @@ import pymongo
 import mongomock
 import os
 
+from app import make_connection
+from app import def_db
+from app import configure_routes
+
 class TestApp:
 
     def test_sanity(self):
         assert True, "Sanity check failed."
+    
+    def test_def_db(self):
+        cxn = make_connection()
+        assert (cxn is not None)
+        db = def_db(cxn)
+        assert (db != -1)
+        print(' *', 'Connected to MongoDB!') # if we get here, the connection worked!
+    
+    def test_base_route():
+        app = configure_routes()
+        client = app.test_client()
+        url = '/'
+        response = client.get(url)
+        assert response.status_code == 200
+    
+    def test_wrong_route():
+        app = configure_routes()
+        client = app.test_client()
+        url = '/test'
+        response = client.get(url)
+        assert response.status_code == 404

7d11c8d3ddb3c0e691bf01a708cb5db187330ca3
markizenlee <xc2097@nyu.edu>
2022-12-18 18:04:31 -0500

    try

--- web-app/tests/test_app.py
+++ web-app/tests/test_app.py
@@ -4,9 +4,7 @@ import pymongo
 import mongomock
 import os
 
-from app import make_connection
-from app import def_db
-from app import configure_routes
+import app as App
 
 class TestApp:
 
@@ -14,35 +12,35 @@ class TestApp:
         assert True, "Sanity check failed."
     
     def test_def_db(self):
-        cxn = make_connection()
+        cxn = App.make_connection()
         assert (cxn is not None)
-        db = def_db(cxn)
+        db = App.def_db(cxn)
         assert (db != -1)
         print(' *', 'Connected to MongoDB!') # if we get here, the connection worked!
     
     def test_base_route(self):
-        app = configure_routes()
+        app = App.configure_routes()
         client = app.test_client()
         url = '/'
         response = client.get(url)
         assert response.status_code == 200
     
     def test_wrong_route(self):
-        app = configure_routes()
+        app = App.configure_routes()
         client = app.test_client()
         url = '/test'
         response = client.get(url)
         assert response.status_code == 404
 
     def test_keyword_route(self):
-        app = configure_routes()
+        app = App.configure_routes()
         client = app.test_client()
         url = "/keyword"
         response = client.get(url)
         assert response.status_code == 200
 
     def test_featured_route(self):
-        app = configure_routes()
+        app = App.configure_routes()
         client = app.test_client()
         url = "/featured"
         response = client.get(url)

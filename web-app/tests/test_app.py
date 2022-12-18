78729812b6baf9eac91d39f7f8a913b43e8be97f
aleolazabal <ajolazabal01@gmail.com>
2022-12-18 14:43:17 -0500

    added testing to webapp

--- web-app/tests/test_app.py
+++ web-app/tests/test_app.py
@@ -1,6 +1,44 @@
 import pytest
 
+import app as App
+
 class TestApp:
 
     def test_sanity(self):
         assert True, "Sanity check failed."
+
+    def test_def_db(self):
+        cxn = App.make_connection()
+        assert (cxn is not None)
+        db = App.def_db(cxn)
+        assert (db != -1)
+        print(' *', 'Connected to MongoDB!') # if we get here, the connection worked!
+
+    def test_base_route(self):
+        app = App.configure_routes()
+        client = app.test_client()
+        url = "/"
+        response = client.get(url)
+        assert response.status_code == 200
+
+    def test_keyword_route(self):
+        app = App.configure_routes()
+        client = app.test_client()
+        url = "/keyword"
+        response = client.get(url)
+        assert response.status_code == 200
+
+    def test_featured_route(self):
+        app = App.configure_routes()
+        client = app.test_client()
+        url = "/featured"
+        response = client.get(url)
+        assert response.status_code == 200
+
+    def test_wrong_route(self):
+        app = App.configure_routes()
+        client = app.test_client()
+        url = "/wrong"
+        response = client.get(url)
+        assert response.status_code == 404
+
